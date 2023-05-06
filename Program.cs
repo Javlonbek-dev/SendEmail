5ab350813d1f737027ef8beedcf55a4b045c3cdd
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-05-06 15:06:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fromMail = "javlonrahmatullayev07@gmail.com";
+            string fromPassword = "qisyjgtdzudxdzoy";
+
+            MailMessage message = new MailMessage();
+            message.From= new MailAddress(fromMail);
+            message.Subject="Test Subjects";
+            message.To.Add(new MailAddress("javlonrahmatullayev07@gmail.com"));
+            message.Body = "<html><body> Test Body </body></html>";
+            message.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true,
+            };
+
+            smtpClient.Send(message);
+        }
+    }
+}
